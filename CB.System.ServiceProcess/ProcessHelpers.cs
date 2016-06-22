f2b3b6e0df2c83f7038571ce0868cd518fec1dd5
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-23 06:29:04 +0700

    Add ProcessHelpers

---
+++ CB.System.ServiceProcess/ProcessHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+
+namespace CB.System.ServiceProcess
+{
+    public static class ProcessHelpers
+    {
+        #region Fields
+        private const string RUN_AS_ADMINISTRATOR = "runas";
+        #endregion
+
+
+        #region Methods
+        public static Process RunAsAdministrator(string fileName, string arguments)
+        {
+            var info = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                Verb = RUN_AS_ADMINISTRATOR
+            };
+            return Process.Start(info);
+        }
+
+        public static async Task RunAsAdministratorAsync(string fileName, string arguments)
+            => await Task.Run(() => RunAsAdministrator(fileName, arguments).WaitForExit());
+
+        public static async Task<bool> RunAsAdministratorAsync(string fileName, string arguments, int milliseconds)
+            => await Task.Run(() => RunAsAdministrator(fileName, arguments).WaitForExit(milliseconds));
+
+        public static void RunAsAdministratorSync(string fileName, string arguments)
+            => RunAsAdministrator(fileName, arguments).WaitForExit();
+
+        public static bool RunAsAdministratorSync(string fileName, string arguments, int milliseconds)
+            => RunAsAdministrator(fileName, arguments).WaitForExit(milliseconds);
+        #endregion
+    }
+}
